157549707e9f68f11bd1e434fc4c9c13c694a01c
Ram Paranjothy <ram.paranjothy@amd.com>
2021-02-16 15:17:41 -0600

    clusterCompute

--- app.py
+++ app.py
@@ -1,59 +1,110 @@
+
 class Load():
     # This is where we define the work?
 
-    def __init__(self,payload):
+    def __init__(self,payload,compute=max):
         self.payload=payload
+        self.compute=compute
 
 
 
-class Controller():
+class Cluster():
     def __init__(self,nProcessors=10,load=None):
         self.nProcessors = nProcessors
 
-        self.incomingWork=load.payload
+        self.incomingPayload=load.payload
+        self.incomingCompute=load.compute
         self.badWorkers = set()
         self.freeWorkers = set()
         self.busyWorkers = set()
         self.bundles=[]
 
         # Start one Leader, rest are workers
-        self.leader= Processor(0)
+        self.leader = Processor(0)
         self.freeWorkers = { Processor(x,isLeader=False) for x in range(1,self.nProcessors)}
         self.splitWork()
-        # TODO: allocate work
+        
+        # Main work aka process
+        if self.bundles==[]:
+            raise Exception("Load not split")
+
+        print("="*40)
+        print(self.incomingCompute.__name__)
+        print("="*40)
+
+        
 
     def splitWork(self):
         '''
             Split the work based on no of workers.. exclude leader
             and get your bundles
         '''
-        if not self.incomingWork:
+        if not self.incomingPayload:
             raise Exception("Empty load")
 
+        # def _split(w,n):
+        #     if w==[]:
+        #         return w
+        #     else:
+        #         self.bundles.append(w[:n])
+        #         return _split(w[n:],n)
+
         def _split(w,n):
+            '''
+                n should be Len of payload/nProcessors
+            '''
             if w==[]:
                 return w
             else:
                 self.bundles.append(w[:n])
-                return _split(w[n:],n)
+                return _split(w[n:],n)                
 
-        _split(self.incomingWork,self.nProcessors-1)
+        # _split(self.incomingPayload,self.nProcessors-1)
+        # max each can get is 4. so iter
+        _split(self.incomingPayload,3)
+
+    def distributeAndCollect(self):
+        '''
+            We have the bundle, we have the computefn, we also have the free workers..
+            so load them up..
+        '''
+        
+        def _assignTaskAndLoad(y):
+            
+            x=self.fetchNextAvailable()
+            self.addToBusy(x)
+            x.assignTask(self.incomingCompute)
+            resFromWorker=x.assignLoadAndRun(y)
+            print(f"{x.name} returned {resFromWorker}")
+            self.addToFree(x)
+            return resFromWorker
+
+        return list(map(_assignTaskAndLoad,self.bundles))
+            
+
 
     def getInventory(self):
         return (self.badWorkers,self.freeWorkers,self.busyWorkers)
+    
+    def getFreeInventory(self):
+        return self.freeWorkers
+
 
     def fetchNextAvailable(self):
         return self.freeWorkers.pop()
 
     def addToFree(self,worker):
         # Take from Busy when done and then add to free
+        
         self.busyWorkers.remove(worker)
         self.freeWorkers.add(worker)
 
 
     def addToBusy(self,worker):
         # Take from Free and then add to busy
-        self.freeWorkers.remove(worker)
+        # Taking from free is via fetchNext
+        # self.freeWorkers.remove(worker)
+
         self.busyWorkers.add(worker)
 
 
@@ -68,12 +119,24 @@ class Processor():
         # sets
         self.id = id
         self.isLeader = isLeader
-        self.name = name
-    
-        # actions
+        self.name = name    
         self.setName()
-        self.info()
         self.dead=False
+        self.task=None
+
+        self.info()
+    
+    def assignTask(self,task):
+        self.task = task
+
+    def assignLoadAndRun(self,payload):    
+        self.payload = payload
+        if not self.task:
+            raise Exception("Task not assigned")
+      
+        out=self.task(self.payload)
+        return out
+    
 
     def isDead(self):
         return self.dead
@@ -97,26 +160,14 @@ class Processor():
    
 def main():
     print("Hello")
-    work=Load([i for i in range(17)])
-    system=Controller(5,work)
-    print(system.bundles)
+    # Define the Work to be distributed here 
+    mainLoad=Load([i for i in range(10)],compute=sum)
+    # Start the cluster with the payload definition and process
+    system=Cluster(3,mainLoad)
+    clusterOut=system.distributeAndCollect()
+    print(clusterOut)
+    
 
 if __name__ == "__main__":
     main()
 
-
- # Not needed - Maintained at Controller level which will be shared
-    # def fetchWorkerInventory(self, controller):
-    #     '''
-
-    #         fetchAllWorkers
-    #         if you become a w>l then you need to know who all are your workers
-    #     '''
-    #     newLeader.allWorkers, newLeader.badWorker, newLeader.freeWorkers, newLeader.busyWorkers = (
-    #         self.allWorkers, self.badWorker, self.freeWorkers, self.busyWorkers)
-
-    # def showWorkerInventory(self):
-    #     """
-    #         Show the worker Inventory under this leaders ownership
-    #     """
-    #     print(self.allWorkers,self.badWorker,self.freeWorkers,self.busyWorkers)
